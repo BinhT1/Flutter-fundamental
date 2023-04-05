85a0cadc24bab9b5f8497c5c5b5d63c8e7c0b6fa
binhT1 <nguyenbasbinh@gmail.com>
2023-04-05 10:25:57 +0700

    Scaffold, Colors & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('hello world'),
-  ));
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red,
+    ),
+    body: const Center(
+      child: Text("Hello world",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: "IndieFlower")),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: Colors.red[600],
+      child: const Text("click"),
+    ),
+  )));
 }
