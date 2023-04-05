e7f10fb2a85d9f7b6547c4be8a9a7482654b532f
binhT1 <nguyenbasbinh@gmail.com>
2023-04-05 11:37:13 +0700

    Image and assets

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,15 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
-      body: const Center(
-        child: Text("Hello world",
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey,
-                fontFamily: "IndieFlower")),
-      ),
+      body: Center(child: Image.asset("assets/img1.jpg"))
+      // Text("Hello world",
+      //     style: TextStyle(
+      //         fontSize: 20,
+      //         fontWeight: FontWeight.bold,
+      //         letterSpacing: 2.0,
+      //         color: Colors.grey,
+      //         fontFamily: "IndieFlower")),
+      ,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
